daf0c5ed2d8b94377a4291bafa410f3c2fa2911f
celujan <123913916+celujan@users.noreply.github.com>
2023-01-30 16:30:52 +0100

    Add files via upload

---
+++ funcionalidades.js
@@ -0,0 +1,19 @@
+console.log("Hola, estoy practicando funcionalidades con JS")
+
+function ConfirmDemo() {
+            var mensaje= confirm("¿Estás de acuerdo con las cualidades propuestas?");
+        if (mensaje) {
+            alert("Gracias por confirmar");
+
+        }
+        else {
+            alert("Haz denegado el mensaje");
+
+        }
+}
+
+
+
+let text = "Amante de mi profesión";
+document.getElementById("myP").innerHTML = text;
+
